2990944e63df870b239e69d97e2f307b7b8e0db5
Choi Bochul <vio.bo94@gmail.com>
2013-09-21 18:18:47 +0900

    Crawler using generator.

---
+++ libearth/crawler-generator/selectcrawler.py
@@ -0,0 +1,93 @@
+import select
+import socket
+from libearth.compat import PY3
+if PY3:
+    import urllib.parse as urlparse
+else:
+    import urlparse
+
+
+class FeedSocket(object):
+
+    just_received = ''
+    content = ''
+
+    def __init__(self, feed_url):
+        try:
+            self.url = feed_url
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            url_parsed = urlparse.urlparse(feed_url)
+            host = url_parsed.netloc
+            path = url_parsed.path
+            self.connect((host, 80))
+            send_buffer = ('GET %s HTTP/1.1\r\n' % path +
+                           'Host: %s\r\n\r\n' % host)
+            sent_len = 0
+            while(sent_len < len(send_buffer)):
+                sent_len = self.sock.send(send_buffer)
+        except:
+            raise ConnectError('Connect to %s failed' % host)
+
+    def __getattr__(self, name):
+        return getattr(self.sock, name)
+
+    def recv(self, length):
+        return self.sock.recv(length)
+
+
+def generator(feed_list):
+    feeds = []
+    reading_pool = []
+    for feed in feed_list:
+        feeds.append(FeedSocket(feed))
+    while feeds or reading_pool:
+        if len(reading_pool) is not 2:
+            while len(reading_pool) is not 2:
+                if feeds:
+                    r, _, _ = select.select(feeds, [], [], 0.5)
+                    if not r:
+                        pass
+                    else:
+                        while r:
+                            if len(reading_pool) is 2:
+                                break
+                            else:
+                                feed = r.pop()
+                                reading_pool.append(feed)
+                                feeds.remove(feed)
+                else:
+                    break
+        if not reading_pool:
+            break
+        else:
+            finished = []
+            has_finished = False
+            while not has_finished:
+                r, _, _ = select.select(reading_pool, [], [], 0.5)
+                if not r:
+                    finished.extend(reading_pool)
+                    has_finished = True
+                for feed in r:
+                    just_received = feed.recv(4096)
+                    if just_received:
+                        feed.content = feed.content + just_received
+                    else:
+                        finished.append(feed)
+                        has_finished = True
+            for feed in finished:
+                reading_pool.remove(feed)
+                yield feed
+
+
+class ConnectError(Exception):
+    """Exception raised when socket connect failed."""
+
+    def __init__(self, msg):
+        self.msg = msg
+
+
+class RecvFinished(Exception):
+    """Exception raised when socket has no data to receive"""
+
+    def __init__(self, msg):
+        self.msg = msg
